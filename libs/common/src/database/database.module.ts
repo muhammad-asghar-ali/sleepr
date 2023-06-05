991a1cd95db429ea10b6eb11a41d4832a2b0a58c
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-06-06 00:09:56 +0500

    feat(): working on abstarct schema and setup database and config module (#1)
    Co-authored-by: apple <apple@192.168.1.26>

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { ConfigurableModuleClass } from "@nestjs/common/cache/cache.module-definition";
+import { ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "../config/config.module";
 
-@Module({})
-export class DatabaseModule {}
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get("MONGODB_URI"),
+      }),  
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule extends ConfigurableModuleClass {}
