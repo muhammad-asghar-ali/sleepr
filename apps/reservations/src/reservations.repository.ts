381ce0c7aaa15a6c17a973a77f7fb84e33cd5f0b
asgr <masgharali.eng@gmail.com>
2023-06-07 00:09:04 +0500

    feat(): working on reservation microservice and complete the reservation servic and setup ts-config settings

---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,16 @@
+import { AbstractRepository } from "@app/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { ReservationDocument } from "./models/reservation.model";
+
+@Injectable()
+export class ReservationRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name) _model: Model<ReservationDocument>
+  ) {
+    super(_model);
+  }
+}
