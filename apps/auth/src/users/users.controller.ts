ccea1dc8bcfe7707d06884fdab13a589ad54554a
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-06-14 18:50:44 +0500

    feat(auth): complete the jwt auth guard and strategy and add some code validations (#7)
    * feat(): working on reservation microservice and complete the reservation servic and setup ts-config settings
    
    * feat(): working on logger and validation for reservation module
    
    * feat(docker): setup docker and docker compose
    
    * feat(auth): working on auth microservice and update the docker-compose
    
    * feat(auth): working on auth micro-service and create guard and decorator and update the docker-compose hash the user password, validate user startegy
    
    * feat(): complete the jwt auth guard and startegy and add some code validations

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,12 +1,22 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
+import { CurrentUser } from "../decorators";
+import { JwtAuthGuard } from "../guards";
 import { CreateUserDto } from "./dto";
+import { UserDocument } from "./models";
 import { UsersService } from "./users.service";
 
 @Controller("users")
 export class UsersController {
-    constructor(private readonly _svc: UsersService) {}
+  constructor(private readonly _svc: UsersService) {}
+
   @Post()
   public async createUser(@Body() createUserDto: CreateUserDto) {
-    return this._svc.create(createUserDto); 
+    return this._svc.create(createUserDto);
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  public async getUser(@CurrentUser() user: UserDocument) {
+    return user;
   }
 }
