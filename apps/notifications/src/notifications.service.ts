36d0d7e0b45308f0fbc264a31b2e94d112f877ee
Muhammad Asghar Ali <51094535+muhammad-asghar-ali@users.noreply.github.com>
2023-06-22 11:03:06 +0500

    feat(): complete notification module (#13)
    * feat(): create notification microservice and connect payment and notification services
    
    * feat(): complete notification module

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import * as nodemailer from 'nodemailer';
 import { NotifyEmailDto } from "./dto";
 
 @Injectable()
 export class NotificationsService {
-  public async notifyEmail({ email }: NotifyEmailDto) {
-    console.log(email);
+  constructor(private readonly configService: ConfigService) {}
+  private readonly transporter =nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: this.configService.get("SMTP_USER"),
+      clientId: this.configService.get("GOOGLE_OAUTH_CLIENT_ID"),
+      clientSecret: this.configService.get("GOOGLE_OAUTH_CLIENT_SECRET"),
+      refreshToken: this.configService.get("GOOGLE_OAUTH_REFRESH_TOKEN"),
+    }
+  })
+  public async notifyEmail({ email, text }: NotifyEmailDto) {
+    await this.transporter.sendMail({
+      from: this.configService.get("SMTP_USER"),
+      to: email,
+      subject: "Sleepr notification",
+      text
+    })
   }
 }
